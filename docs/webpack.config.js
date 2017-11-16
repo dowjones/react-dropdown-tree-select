4e66dba216ec04cdd592a47d7456dfa7e5df180b
mrchief <mrchief_2000@yahoo.com>
2017-11-15 22:42:23 -0500

    docs(demo): Unified webpack config for all 3 examples plus webpack-dev-server build for demo :memo:

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,31 +1,56 @@
-module.exports = {
+const path = require('path')
+
+const baseConfig = {
   devtool: 'eval',
-  entry: __dirname,
-  output: {
-    path: __dirname,
-    filename: 'bundle.js'
-  },
   resolve: {
     extensions: ['.js', '.css'],
     modules: ['node_modules']
   },
   module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader'
-          }
-        ],
-        include: /src/
-      }
-    ]
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    },
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader'
+        }
+      ],
+      include: /src/
+    }]
+  },
+  devServer: {
+    contentBase: './docs',
+    port: 3000,
+    historyApiFallback: true,
+    open: true,
+    watchContentBase: true
   }
 }
+
+module.exports = [{
+  ...baseConfig,
+  entry: __dirname,
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  }
+}, {
+  ...baseConfig,
+  entry: path.join(__dirname, 'examples/bootstrap'),
+  output: {
+    path: path.join(__dirname, 'examples/bootstrap'),
+    filename: 'bundle.js'
+  }
+}, {
+  ...baseConfig,
+  entry: path.join(__dirname, 'examples/material'),
+  output: {
+    path: path.join(__dirname, 'examples/material'),
+    filename: 'bundle.js'
+  }
+}]

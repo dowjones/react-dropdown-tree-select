f75f389f9c5b4320bd8d8d9d36246cb842e75e3a
Hrusikesh Panda <mrchief_2000@yahoo.com>
2018-03-25 22:23:20 -0400

    test: Add tests for checkbox

--- src/checkbox/index.js
+++ src/checkbox/index.js
@@ -1,21 +1,16 @@
 import React from 'react'
 
+export const refUpdater = ({ checked, indeterminate }) => input => {
+  if (input) {
+    input.checked = checked
+    input.indeterminate = indeterminate
+  }
+}
+
 const Checkbox = props => {
   const { className, checked, indeterminate = false, onChange, ...rest } = props
-  return (
-    <input
-      type="checkbox"
-      className={className}
-      ref={input => {
-        if (input) {
-          input.checked = checked
-          input.indeterminate = indeterminate
-        }
-      }}
-      onChange={onChange}
-      {...rest}
-    />
-  )
+
+  return <input type="checkbox" className={className} ref={refUpdater({ checked, indeterminate })} onChange={onChange} {...rest} />
 }
 
 export default Checkbox
---
+++ src/checkbox/index.test.js
@@ -0,0 +1,29 @@
+import { shallow, mount } from 'enzyme'
+import { spy } from 'sinon'
+import React from 'react'
+import test from 'ava'
+import toJson from 'enzyme-to-json'
+
+import Checkbox, { refUpdater } from './index'
+
+test('Checkbox component', t => {
+  const tree = toJson(shallow(<Checkbox className="sample" />))
+  t.snapshot(tree)
+})
+
+test('renders indeterminate state', t => {
+  const input = {}
+  refUpdater({ indeterminate: true })(input)
+  t.true(input.indeterminate)
+})
+
+test('renders checked state', t => {
+  const input = {}
+  refUpdater({ checked: true })(input)
+  t.true(input.checked)
+})
+
+test('renders disabled state', t => {
+  const tree = toJson(shallow(<Checkbox disabled />))
+  t.snapshot(tree)
+})

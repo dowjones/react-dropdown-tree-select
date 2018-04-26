692d9ff644f77608113071010301fef7baadd373
Hrusikesh Panda <mrchief_2000@yahoo.com>
2018-04-25 22:06:46 -0400

    fix: Add polyfill for Event.path

--- src/utils/isOutsideClick.js
+++ src/utils/isOutsideClick.js
@@ -1,4 +1,21 @@
+const getPath = e => {
+  if (e.path) return e.path
+
+  const path = [e]
+
+  if (!e || !e.parentElement) {
+    return []
+  }
+
+  while (e.parentElement) {
+    e = e.parentElement
+    path.unshift(e)
+  }
+
+  return path
+}
+
 export default e => {
   if (!(e instanceof Event)) return false
-  return !e.path.some(node => node.className && node.className.indexOf('react-dropdown-tree-select') >= 0)
+  return !getPath(e).some(node => node.className && node.className.indexOf('react-dropdown-tree-select') >= 0)
 }

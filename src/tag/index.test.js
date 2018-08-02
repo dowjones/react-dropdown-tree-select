18ab0d926cb32fc7299fbb2b65eb6949be257b20
PRIYANSHU AGRAWAL <priyanshuagrawal@outlook.com>
2018-08-03 03:32:03 +0530

    fix: Do not toggle dropwdown on pill removal (#141) :bug:
    fix: Do not toggle dropwdown on pill removal

--- src/tag/index.test.js
+++ src/tag/index.test.js
@@ -6,15 +6,33 @@ import toJson from 'enzyme-to-json'
 
 import Tag from './index'
 
+const nativeEvent = { nativeEvent: { stopImmediatePropagation: () => {} } }
+
 test('renders label when passed in', t => {
   const wrapper = toJson(shallow(<Tag label="hello" id="abc" />))
   t.snapshot(wrapper)
 })
 
+test('call stopPropagation and stopImmediatePropagation when pill is closed', t => {
+  const onDelete = spy()
+  const wrapper = mount(<Tag label="hello" id="abc" onDelete={onDelete} />)
+  const event = {
+    type: 'click',
+    stopPropagation: spy(),
+    nativeEvent: {
+      stopImmediatePropagation: spy()
+    }
+  }
+  wrapper.find('.tag-remove').prop('onClick')(event)
+  t.true(event.stopPropagation.called)
+  t.true(event.nativeEvent.stopImmediatePropagation.called)
+})
+
+
 test('call onDelete handler when pill is closed', t => {
   const onDelete = spy()
   const wrapper = mount(<Tag label="hello" id="abc" onDelete={onDelete} />)
-  wrapper.find('.tag-remove').simulate('click')
+  wrapper.find('.tag-remove').simulate('click', nativeEvent)
   t.true(onDelete.calledWith('abc'))
 })
 
@@ -24,6 +42,6 @@ test('should not cause form submit', t => {
   const wrapper = mount(<form onSubmit={onSubmit}>
     <Tag label="hello" id="abc" onDelete={onDelete} />
   </form>)
-  wrapper.find('.tag-remove').simulate('click')
+  wrapper.find('.tag-remove').simulate('click', nativeEvent)
   t.false(onSubmit.called)
 })

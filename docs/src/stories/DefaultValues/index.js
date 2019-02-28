8374cfcd1216515cc648c60306e61ac836554baa
Stoyan Berov <stoyan.berov@gmail.com>
2019-02-28 20:55:47 +0200

    fix: Undo commit d83a676
    - styles.css file gets generated after running 'yarn build'

--- docs/src/stories/DefaultValues/index.js
+++ docs/src/stories/DefaultValues/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import DropdownTreeSelect from '../../../../src'
-// import '../../../../dist/styles.css'
+import '../../../../dist/styles.css'
 
 import './index.css'
 import data from './data.json'

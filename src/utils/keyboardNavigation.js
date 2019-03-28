244a573fb18d2a740f749cc921d523fcf0c6a40a
Magnus Ellinge <magnus.ellinge@consid.se>
2019-03-28 01:13:10 +0100

    fix: Adjust code to check codeclimate

--- src/utils/keyboardNavigation.js
+++ src/utils/keyboardNavigation.js
@@ -21,43 +21,42 @@ export const NavActions = {
   ToggleChecked: 'ToggleChecked'
 }
 
-const keyboardNavigationuUtil = () => {
-  const validTriggerOpenKeys = [Keys.Up, Keys.Down, Keys.Home, Keys.PageUp, Keys.End, Keys.PageDown]
-  const validKeys = validTriggerOpenKeys.concat([Keys.Left, Keys.Right, Keys.Enter])
+const validTriggerOpenKeys = [Keys.Up, Keys.Down, Keys.Home, Keys.PageUp, Keys.End, Keys.PageDown]
+const validKeys = validTriggerOpenKeys.concat([Keys.Left, Keys.Right, Keys.Enter])
 
-  const isValidKey = (key, isOpen) => {
-    const keysToCheck = isOpen ? validKeys : validTriggerOpenKeys
-    return keysToCheck.indexOf(key) > -1
-  }
-
-  const getAction = (currentFocus, key) => {
-    switch (key) {
-      case Keys.Up:
-        return currentFocus ? NavActions.FocusPrevious : NavActions.FocusLast
-      case Keys.Down:
-        return currentFocus ? NavActions.FocusNext : NavActions.FocusFirst
-      case Keys.Left:
-        if (!currentFocus) return NavActions.None
-        if (currentFocus.expanded) return NavActions.ToggleExpanded
-        return currentFocus._parent ? NavActions.FocusParent : NavActions.None
-      case Keys.Right:
-        if (!currentFocus || !currentFocus._children) return NavActions.None
-        return currentFocus.expanded === true ? NavActions.FocusNext : NavActions.ToggleExpanded
-      case Keys.Enter:
-        return currentFocus ? NavActions.ToggleChecked : NavActions.None
-      case Keys.Home:
-      case Keys.PageUp:
-        return NavActions.FocusFirst
-      case Keys.End:
-      case Keys.PageDown:
-        return NavActions.FocusLast
-      default:
-        return NavActions.None
-    }
-  }
-
-  return { isValidKey, getAction }
+const isValidKey = (key, isOpen) => {
+  const keysToCheck = isOpen ? validKeys : validTriggerOpenKeys
+  return keysToCheck.indexOf(key) > -1
 }
 
-const keyboardNavigation = keyboardNavigationuUtil()
+const getAction = (currentFocus, key) => {
+  switch (key) {
+    case Keys.Up:
+      return currentFocus ? NavActions.FocusPrevious : NavActions.FocusLast
+    case Keys.Down:
+      return currentFocus ? NavActions.FocusNext : NavActions.FocusFirst
+    case Keys.Left:
+      if (!currentFocus) return NavActions.None
+      if (currentFocus.expanded) return NavActions.ToggleExpanded
+      return currentFocus._parent ? NavActions.FocusParent : NavActions.None
+    case Keys.Right:
+      if (!currentFocus || !currentFocus._children) return NavActions.None
+      return currentFocus.expanded === true ? NavActions.FocusNext : NavActions.ToggleExpanded
+    case Keys.Enter:
+      return currentFocus ? NavActions.ToggleChecked : NavActions.None
+    case Keys.Home:
+    case Keys.PageUp:
+      return NavActions.FocusFirst
+    case Keys.End:
+    case Keys.PageDown:
+      return NavActions.FocusLast
+    default:
+      return NavActions.None
+  }
+}
+
+const keyboardNavigation = {
+  isValidKey,
+  getAction
+}
 export default keyboardNavigation

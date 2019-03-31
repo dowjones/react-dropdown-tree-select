fb74cec2bc5b61eea6bb0bc8ec486df135dc4bb5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-31 17:00:31 +0000

    Deploy dowjones/react-dropdown-tree-select to github.com/dowjones/react-dropdown-tree-select.git:gh-pages

---
+++ src/stories/Options/index.js
@@ -0,0 +1,80 @@
+import React, { PureComponent } from 'react'
+
+import Checkbox from './Checkbox'
+import DropdownTreeSelect from '../../../../src'
+
+import './index.css'
+import data from './data.json'
+
+class WithOptions extends PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      clearSearchOnChange: false,
+      keepTreeOnSearch: false,
+      simpleSelect: false,
+      showPartiallySelected: false,
+      disabled: false,
+      readOnly: false,
+      hierarchical: false
+    }
+  }
+
+  onChange = (curNode, selectedNodes) => {
+    console.log('onChange::', curNode, selectedNodes)
+  }
+  onAction = ({ action, id }) => {
+    console.log(`onAction:: [${action}]`, id)
+  }
+  onNodeToggle = curNode => {
+    console.log('onNodeToggle::', curNode)
+  }
+
+  onOptionsChange = value => {
+    this.setState({ [value]: !this.state[value] })
+  }
+
+  render() {
+    const { clearSearchOnChange, keepTreeOnSearch, simpleSelect, showPartiallySelected, disabled, readOnly, hierarchical } = this.state
+
+    return (
+      <div>
+        <h1>Options playground</h1>
+        <p>Toggle different options to see its effect on the dropdown.</p>
+        <div
+          style={{
+            border: '1px solid #ccc',
+            borderRadius: '4px',
+            marginBottom: '20px',
+            padding: 10
+          }}
+        >
+          <Checkbox label="Clear search on selection" value="clearSearchOnChange" checked={clearSearchOnChange} onChange={this.onOptionsChange} />
+          <Checkbox label="Keep tree on search" value="keepTreeOnSearch" checked={keepTreeOnSearch} onChange={this.onOptionsChange} />
+          <Checkbox label="Simple Select" value="simpleSelect" checked={simpleSelect} onChange={this.onOptionsChange} />
+          <Checkbox label="Show Partially Selected" value="showPartiallySelected" checked={showPartiallySelected} onChange={this.onOptionsChange} />
+          <Checkbox label="Disabled" value="disabled" checked={disabled} onChange={this.onOptionsChange} />
+          <Checkbox label="Read Only" value="readOnly" checked={readOnly} onChange={this.onOptionsChange} />
+          <Checkbox label="Hierarchical" value="hierarchical" checked={hierarchical} onChange={this.onOptionsChange} />
+        </div>
+        <div>
+          <DropdownTreeSelect
+            data={data}
+            onChange={this.onChange}
+            onAction={this.onAction}
+            onNodeToggle={this.onNodeToggle}
+            clearSearchOnChange={clearSearchOnChange}
+            keepTreeOnSearch={keepTreeOnSearch}
+            simpleSelect={simpleSelect}
+            showPartiallySelected={showPartiallySelected}
+            disabled={disabled}
+            readOnly={readOnly}
+            hierarchical={hierarchical}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+export default WithOptions

0a89cd39b31e3d9d769681c3cc68312f99f8f7ec
Magnus Ellinge <magnus.ellinge@consid.se>
2019-05-02 00:49:27 +0200

    fix: Add comment for removing hash

--- src/a11y/index.js
+++ src/a11y/index.js
@@ -2,6 +2,7 @@ export function getAriaLabel(label) {
   if (!label) return undefined
 
   if (label && label[0] === '#') {
+    // If input label reference ids (hash), use a labelledby without hashes
     return { 'aria-labelledby': label.replace(/#/g, '') }
   }
   return { 'aria-label': label }

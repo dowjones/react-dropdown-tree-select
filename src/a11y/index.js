791ff7576fdafa934248cff3bfd8a6f46095dbc5
Magnus Ellinge <magnus.ellinge@dom.se>
2019-05-02 09:44:51 +0200

    fix: Expand on comment for hash replacement

--- src/a11y/index.js
+++ src/a11y/index.js
@@ -3,7 +3,7 @@ export function getAriaLabel(label) {
 
   if (label && label[0] === '#') {
     /* See readme for label. When label starts with # it references ids of dom nodes instead.
-      When used on aria-labelledby, they should reference the nodes with a starting hash/# */
+      When used on aria-labelledby, they should be referenced without a starting hash/# */
     return { 'aria-labelledby': label.replace(/#/g, '') }
   }
   return { 'aria-label': label }

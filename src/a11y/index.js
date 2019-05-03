7133ed23040a85dac798a40d2e83f35c121ec276
Gustav Tonér <gustav.toner@gmail.com>
2019-05-03 04:48:22 +0200

    feat: Add ARIA roles and attributes (#233)
    Fixes #235 
    Fixes #216

--- src/a11y/index.js
+++ src/a11y/index.js
@@ -1,7 +1,9 @@
 export function getAriaLabel(label) {
   if (!label) return undefined
 
-  if (label && label.length && label[0] === '#') {
+  if (label && label[0] === '#') {
+    /* See readme for label. When label starts with # it references ids of dom nodes instead.
+      When used on aria-labelledby, they should be referenced without a starting hash/# */
     return { 'aria-labelledby': label.replace(/#/g, '') }
   }
   return { 'aria-label': label }
--- src/tree-node/index.js
+++ src/tree-node/index.js
@@ -76,6 +76,21 @@ class TreeNode extends PureComponent {
     clientId: PropTypes.string,
   }
 
+  getAriaAttributes = () => {
+    const { _children, _depth, checked, disabled, expanded, readOnly, simpleSelect, partial } = this.props
+    const attributes = {}
+
+    attributes.role = simpleSelect ? 'option' : 'treeitem'
+    attributes['aria-disabled'] = disabled || readOnly
+    attributes['aria-selected'] = checked
+    if (!simpleSelect) {
+      attributes['aria-checked'] = partial ? 'mixed' : checked
+      attributes['aria-level'] = (_depth || 0) + 1
+      attributes['aria-expanded'] = _children && (expanded ? 'true' : 'false')
+    }
+    return attributes
+  }
+
   render() {
     const {
       simpleSelect,
@@ -107,7 +122,7 @@ class TreeNode extends PureComponent {
     const liId = `${_id}_li`
 
     return (
-      <li className={liCx} style={style} {...getDataset(dataset)} id={liId}>
+      <li className={liCx} style={style} id={liId} {...getDataset(dataset)} {...this.getAriaAttributes()}>
         <Toggle isLeaf={isLeaf(_children)} expanded={expanded} id={_id} onNodeToggle={onNodeToggle} />
         <NodeLabel
           title={title}

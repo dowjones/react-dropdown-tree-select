f273777b63da3aa4112f181d03ee7a6cc9bc55de
Hrusikesh Panda <hrusikesh.panda@dowjones.com>
2019-10-12 12:09:14 -0400

    chore: Convert to functional component

--- src/checkbox/index.js
+++ src/checkbox/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 export const refUpdater = ({ checked, indeterminate }) => input => {
@@ -8,30 +8,29 @@ export const refUpdater = ({ checked, indeterminate }) => input => {
   }
 }
 
-class Checkbox extends PureComponent {
-  static propTypes = {
-    checked: PropTypes.bool,
-    indeterminate: PropTypes.bool,
-    onChange: PropTypes.func,
-    disabled: PropTypes.bool,
-    readOnly: PropTypes.bool,
-  }
+const Checkbox = props => {
+  const { checked, indeterminate = false, onChange, disabled, readOnly, ...rest } = props
+  const isDisabled = disabled || readOnly
+  return (
+    <input
+      type="checkbox"
+      ref={refUpdater({
+        checked,
+        indeterminate,
+      })}
+      onChange={onChange}
+      disabled={isDisabled}
+      {...rest}
+    />
+  )
+}
 
-  render() {
-    const { checked, indeterminate = false, onChange, disabled, readOnly, ...rest } = this.props
-
-    const isDisabled = disabled || readOnly
-
-    return (
-      <input
-        type="checkbox"
-        ref={refUpdater({ checked, indeterminate })}
-        onChange={onChange}
-        disabled={isDisabled}
-        {...rest}
-      />
-    )
-  }
+Checkbox.propTypes = {
+  checked: PropTypes.bool,
+  indeterminate: PropTypes.bool,
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
+  readOnly: PropTypes.bool,
 }
 
 export default Checkbox

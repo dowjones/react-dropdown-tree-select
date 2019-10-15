e868935d21440ea7f7807b4ee3fb031b3e56b759
Hrusikesh Panda <hrusikesh.panda@dowjones.com>
2019-10-14 22:44:23 -0400

    chore: Convert to functional component

--- src/tree-node/node-label.js
+++ src/tree-node/node-label.js
@@ -1,6 +1,6 @@
 import cn from 'classnames/bind'
 import PropTypes from 'prop-types'
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 import Checkbox from '../checkbox'
 import RadioButton from '../radio'
 
@@ -8,71 +8,67 @@ import styles from './index.css'
 
 const cx = cn.bind(styles)
 
-class NodeLabel extends PureComponent {
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    actions: PropTypes.array,
-    title: PropTypes.string,
-    label: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    checked: PropTypes.bool,
-    partial: PropTypes.bool,
-    disabled: PropTypes.bool,
-    dataset: PropTypes.object,
-    mode: PropTypes.oneOf(['multiSelect', 'simpleSelect', 'radioSelect', 'hierarchical']),
-    showPartiallySelected: PropTypes.bool,
-    onCheckboxChange: PropTypes.func,
-    readOnly: PropTypes.bool,
-    clientId: PropTypes.string,
-  }
+const NodeLabel = props => {
+  const { mode, title, label, id, partial, checked, onCheckboxChange } = props
+  const { value, disabled, showPartiallySelected, readOnly, clientId } = props
+  const nodeLabelProps = { className: 'node-label' }
 
-  handleCheckboxChange = e => {
-    const { mode, id, onCheckboxChange } = this.props
-
+  const handleCheckboxChange = e => {
     if (mode === 'simpleSelect' || mode === 'radioSelect') {
       onCheckboxChange(id, true)
     } else {
-      const {
-        target: { checked },
-      } = e
-      onCheckboxChange(id, checked)
+      onCheckboxChange(id, e.target.checked)
     }
+
     e.stopPropagation()
     e.nativeEvent.stopImmediatePropagation()
   }
 
-  render() {
-    const { mode, title, label, id, partial, checked } = this.props
-    const { value, disabled, showPartiallySelected, readOnly, clientId } = this.props
-    const nodeLabelProps = { className: 'node-label' }
-
-    // in case of simple select mode, there is no checkbox, so we need to handle the click via the node label
-    // but not if the control is in readOnly or disabled state
-    const shouldRegisterClickHandler = mode === 'simpleSelect' && !readOnly && !disabled
-
-    if (shouldRegisterClickHandler) {
-      nodeLabelProps.onClick = this.handleCheckboxChange
-    }
-
-    const sharedProps = { id, value, checked, disabled, readOnly, tabIndex: -1 }
-
-    return (
-      <label title={title || label} htmlFor={id}>
-        {mode === 'radioSelect' ? (
-          <RadioButton name={clientId} className="radio-item" onChange={this.handleCheckboxChange} {...sharedProps} />
-        ) : (
-          <Checkbox
-            name={id}
-            className={cx('checkbox-item', { 'simple-select': mode === 'simpleSelect' })}
-            indeterminate={showPartiallySelected && partial}
-            onChange={this.handleCheckboxChange}
-            {...sharedProps}
-          />
-        )}
-        <span {...nodeLabelProps}>{label}</span>
-      </label>
-    )
+  // in case of simple select mode, there is no checkbox, so we need to handle the click via the node label
+  // but not if the control is in readOnly or disabled state
+  if (mode === 'simpleSelect' && !readOnly && !disabled) {
+    nodeLabelProps.onClick = handleCheckboxChange
   }
+
+  const sharedProps = {
+    id,
+    value,
+    checked,
+    disabled,
+    readOnly,
+    tabIndex: -1,
+  }
+  return (
+    <label title={title || label} htmlFor={id}>
+      {mode === 'radioSelect' ? (
+        <RadioButton name={clientId} className="radio-item" onChange={handleCheckboxChange} {...sharedProps} />
+      ) : (
+        <Checkbox
+          name={id}
+          className={cx('checkbox-item', { 'simple-select': mode === 'simpleSelect' })}
+          indeterminate={showPartiallySelected && partial}
+          onChange={handleCheckboxChange}
+          {...sharedProps}
+        />
+      )}
+      <span {...nodeLabelProps}>{label}</span>
+    </label>
+  )
 }
 
-export default NodeLabel
+NodeLabel.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  checked: PropTypes.bool,
+  partial: PropTypes.bool,
+  disabled: PropTypes.bool,
+  mode: PropTypes.oneOf(['multiSelect', 'simpleSelect', 'radioSelect', 'hierarchical']),
+  showPartiallySelected: PropTypes.bool,
+  onCheckboxChange: PropTypes.func,
+  readOnly: PropTypes.bool,
+  clientId: PropTypes.string,
+}
+
+export default memo(NodeLabel)

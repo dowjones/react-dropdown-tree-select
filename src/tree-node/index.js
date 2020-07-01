12277c5865e6cbbee3f5ef9020fa492b44e1119e
Akarsh Jairaj <akarshjairaj@gmail.com>
2020-07-01 09:37:06 +0530

    Fixed padding of leaf nodes for all levels

--- src/tree-node/index.js
+++ src/tree-node/index.js
@@ -112,7 +112,7 @@ class TreeNode extends PureComponent {
       clientId,
     } = this.props
     const liCx = getNodeCx(this.props)
-    const style = keepTreeOnSearch || !searchModeOn ? { paddingLeft: `${(_depth || 0) * 20}px` } : {}
+    const style = keepTreeOnSearch || !searchModeOn ? { paddingLeft: `${(_depth || 0) * 20 + (isLeaf(_children)?15:0)}px` } : {}
 
     const liId = `${_id}_li`
 
